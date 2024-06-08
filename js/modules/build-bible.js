5a0140f3feac982c1bf54b430bd40439576545be
Joabson-developer <joabson_dev.front@hotmail.com>
2024-06-08 20:23:46 -0300

    feat: adiciona paginacao

--- js/modules/build-bible.js
+++ js/modules/build-bible.js
@@ -97,6 +97,51 @@ export function buildBible(data) {
 
     document.querySelector(".l-main").innerHTML = bibleTextElement
 
+    const footerButtonLeft = document.getElementById("footerButtonLeft")
+    if (queryParams.get("chapter") === "1") {
+      footerButtonLeft.setAttribute("disabled", "true")
+      footerButtonLeft.setAttribute(
+        "aria-label",
+        "Voltar para o capítulo anterior está desabilitado porque você já está no capítulo 1"
+      )
+      footerButtonLeft.dataset.href = ""
+    } else {
+      footerButtonLeft.removeAttribute("disabled")
+      footerButtonLeft.setAttribute(
+        "aria-label",
+        `Voltar para o capítulo ${Number(queryParams.get("chapter")) - 1}`
+      )
+      footerButtonLeft.dataset.href = `/?version=${queryParams.get(
+        "version"
+      )}&book=${queryParams.get("book")}&chapter=${
+        Number(queryParams.get("chapter")) - 1
+      }`
+    }
+
+    const footerButtonRight = document.getElementById("footerButtonRight")
+    if (queryParams.get("chapter") === `${getBook.chapters}`) {
+      footerButtonRight.setAttribute("disabled", "true")
+      footerButtonRight.setAttribute(
+        "aria-label",
+        `Avançar para o próximo capítulo está desabilitado porque o capítulo ${getBook.chapters} é o limite`
+      )
+      footerButtonRight.dataset.href = ""
+    } else {
+      footerButtonRight.removeAttribute("disabled")
+      footerButtonRight.setAttribute(
+        "aria-label",
+        `Avançar para o capítulo ${Number(queryParams.get("chapter")) + 1}`
+      )
+      footerButtonRight.dataset.href = `/?version=${queryParams.get(
+        "version"
+      )}&book=${queryParams.get("book")}&chapter=${
+        Number(queryParams.get("chapter")) + 1
+      }`
+    }
+
+    const FooterBook = document.getElementById("FooterBook")
+    FooterBook.innerText = `${getBook.book_name} ${queryParams.get("chapter")}`
+
     loading(false)
 
     const bookButtons = document.querySelectorAll(".c-book")
@@ -104,7 +149,7 @@ export function buildBible(data) {
 
     return {
       bookButtons,
-      chapterButtons
+      chapterButtons: [...chapterButtons, footerButtonLeft, footerButtonRight]
     }
   }
 }
