7a29b8ed0c448cc68c159caaa339678794f06ed8
Joabson-developer <joabson_dev.front@hotmail.com>
2024-06-14 17:55:46 -0300

    feat: cria listagem de favoritos

--- js/favorites.js
+++ js/favorites.js
@@ -1,11 +1,30 @@
+import "./modules/bottom-navigation.js"
 import { LOCAL_PREFIX } from "./modules/consts/local-prefix.js"
 
 try {
   const data = window.localStorage.getItem(`${LOCAL_PREFIX}:favorites`)
 
   if (data) {
-    const favorites = JSON.parse(data)
-    console.log(favorites)
+    const favorites = JSON.parse(data) || []
+    const elements = favorites
+      .map(({ verse, href }) => {
+        const [verseText, verseReference] = verse.split("\n")
+
+        return `
+        <li class="c-favorite-list__item">
+          <a href="${href}">
+            <p class="c-favorite-list__item__text">
+              <i class="fa-solid fa-book-bible"></i>
+              ${verseText}
+            </p>
+            <span class="c-favorite-list__item__reference">${verseReference}</span>
+          </a>
+        </li>
+      `
+      })
+      .join("\n")
+
+    document.querySelector(".c-favorite-list").innerHTML = elements
   } else throw new Error("No data")
 } catch (error) {
   console.error(error)
--- js/modules/bottom-navigation.js
+++ js/modules/bottom-navigation.js
@@ -1,9 +1,15 @@
+import { LOCAL_PREFIX } from "./consts/local-prefix.js"
 import { queryParams } from "./utils/query-params.js"
 
 const bottomNavigation = document.querySelectorAll("[data-callback]")
 
 const actions = {
+  home() {
+    const homeHref = window.sessionStorage.getItem(`${LOCAL_PREFIX}:home`)
+    window.location.href = homeHref || "/"
+  },
   favorites() {
+    window.sessionStorage.setItem(`${LOCAL_PREFIX}:home`, window.location.href)
     window.location.href = `/favorites.html?version=${
       queryParams.get("version") || "acf"
     }`
